3b3e929bbf7c27dceb7bb69065091a871b2f328c
honeyamo <62083643+honeyamo@users.noreply.github.com>
2020-03-21 03:10:57 -0700

    Add files via upload

---
+++ Customer.java
@@ -0,0 +1,16 @@
+public class Customer {
+	private String brand;
+	private String name;
+	
+	/**
+	 * Constructor for the Customer class
+	 * 
+	 * @param brand the cosmetic brand
+	 * @param name the cosmetic name
+	 */
+	public Customer(String brand, String name) {
+		this.brand = brand;
+		this.name = name;
+	}
+	
+}
